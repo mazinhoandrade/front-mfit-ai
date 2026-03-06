dad2e23b43901cad04b6a884eaf509cf80907e25
Mazinho <irismarandrade94@gmail.com>
2026-03-05 23:21:01 -0300

    refactor: Refatorar a estrutura do código para melhorar a legibilidade e a capacidade de manutenção.

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,41 @@
-"use client";
 
 import { authClient } from "@/app/_lib/auth-client";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import Image from "next/image";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { getHomeData } from "./_lib/api/fetch-generated";
+import dayjs from "dayjs";
 
-export default function Home() {
-  const { data: session, isPending } = authClient.useSession();
-  const router = useRouter();
 
-  useEffect(() => {
-    if (!isPending && !session) {
-      router.push("/auth");
+export default async function Home() {
+  const session = await authClient.getSession({
+    fetchOptions: {
+      headers: await headers(),
     }
-  }, [session, isPending, router]);
+  });
+  
+  if (!session.data?.user) redirect("/auth");
 
-  if (isPending) {
-    return (
-      <div className="flex min-h-screen items-center justify-center bg-black text-white">
-        Carregando...
-      </div>
-    );
-  }
+  const homeData = await getHomeData(dayjs().format("2026-03-05"));
 
-  if (!session) {
-    return null; // Will redirect via useEffect
-  }
+
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
+        Mfit.ai
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Você está logado como {session.user.name}
+            Você está logado como {session.data?.user.name}
           </h1>
-          <button
+          <div>
+           
+          </div>
+          {/* <button
             onClick={() => authClient.signOut()}
             className="rounded-full bg-red-500 px-4 py-2 text-white hover:bg-red-600"
           >
             Sair
-          </button>
+          </button> */}
         </div>
       </main>
     </div>
